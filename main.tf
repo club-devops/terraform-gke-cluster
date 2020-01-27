79017b74cc0bcf39e4d76f14fa41add01c02f026
Oleksandr Zaichenko <allex@Administrators-MacBook-Pro-2.local>
2020-01-27 21:53:38 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+# This data source fetches the project name, and provides the appropriate URLs to use for container registry for this project.
+# https://www.terraform.io/docs/providers/google/d/google_container_registry_repository.html
+data "google_container_registry_repository" "registry" {
+  project                   = var.project
+}
+
+# Provides access to available Google Container Engine versions in a zone for a given project.
+# https://www.terraform.io/docs/providers/google/d/google_container_engine_versions.html
+data "google_container_engine_versions" "engine_version" {
+  project                   = var.project
+  location                  = var.zone
+}
+
+resource "google_container_cluster" "primary" {
+  provider                  = google-beta
+  name                      = var.name
+  description               = var.description
+  #zonal cluster
+  location                  = var.zone
+  initial_node_count        = var.initial_node_count
+  remove_default_node_pool  = "true"
+  project                   = var.project
+  default_max_pods_per_node = var.default_max_pods_per_node
+  min_master_version        = data.google_container_engine_versions.engine_version.latest_master_version
+  node_version              = data.google_container_engine_versions.engine_version.latest_node_version
+
+  addons_config {
+    kubernetes_dashboard {
+      disabled              = var.disable_dashboard
+    }
+
+    http_load_balancing {
+      disabled              = var.disable_autoscaling_addon
+    }
+  }
+
+  network                   = var.network
+  subnetwork                = var.subnetwork
+
+  ip_allocation_policy {
+    #use_ip_aliases          = "false"
+    services_ipv4_cidr_block = var.services_network
+    node_ipv4_cidr_block     = var.nodes_network
+    create_subnetwork        = "false"
+    cluster_ipv4_cidr_block  = var.pods_network
+
+  }
+#cluster_ipv4_cidr - The IP address range of the kubernetes pods in this cluster. Default is an automatically assigned CIDR
+# cluster_ipv4_cidr = "${var.pods_network}"
+
+  private_cluster_config {
+    enable_private_endpoint = false
+    enable_private_nodes    = true
+    master_ipv4_cidr_block  = "172.16.220.0/28"
+  }
+
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = "03:00"
+    }
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,74 @@
+
+variable "name" {
+  description = "The name of the cluster, unique within the project and zone"
+  default     = "primary"
+}
+
+variable "project" {
+  description = "project ID"
+}
+
+variable "description" {
+  description = "Cluster description"
+}
+
+variable "zone" {
+  description = "The zone the master and nodes specified in initial_node_count should be created in"
+}
+
+variable "disable_dashboard" {
+  description = "Whether the Kubernetes Dashboard should be disabled"
+  default     = false
+}
+
+variable "disable_autoscaling_addon" {
+  description = "Whetherthe Autoscaling Pod addon should be disabled"
+  default     = false
+}
+
+variable "initial_node_count" {
+  description = "The number of nodes to create in this cluster (not including the Kubernetes master)"
+  default     = 1
+}
+
+variable "network" {
+  description = "The name or self_link of the Google Compute Engine network to which the cluster is connected"
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "The name of the Google Compute Engine network subnetwork"
+  default     = "cluster-subnetwork"
+
+}
+
+variable "nodes_network" {
+  description = "Nodes network"
+  default = "10.50.25.0/24"
+}
+
+variable "pods_network" {
+  description = "Pods network"
+  default = "10.50.128.0/18"
+}
+
+variable "services_network" {
+  description = "Services network"
+  default = "10.50.192.0/18"
+
+}
+
+variable "node_disk_size_gb" {
+  description = "Size of the disk attached to each node, specified in GB"
+  default     = 10
+}
+
+variable "node_machine_type" {
+  description = "The name of a Google Compute Engine machine type"
+  default     = "n1-standard-1"
+}
+
+variable "default_max_pods_per_node" {
+  description = "Max pods per node"
+  default = 32
+}
